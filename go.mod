5bec0096e0a70302d4a6597beea40c0840c0768f
tanmaypanat <panattanmay@gmail.com>
2025-10-02 23:45:56 -0400

    Initial commit: Hello World Go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tanmaypanat/distributed-go-platform
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, Klaviyo! 🚀 This is Tanmay’s distributed platform project.")
+}
